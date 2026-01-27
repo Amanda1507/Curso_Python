99019272f227fb49db77e245ca7c776981f45065
Amanda1507 <lilhithrose@gmail.com>
2026-01-27 14:41:00 -0300

    Add files via upload

---
+++ exercicios-5/ex1.py
@@ -0,0 +1,7 @@
+nota = int(input("Digite uma nota entre 0 á 10: "))
+
+for i in range (1,99999999999999999999):
+    if nota > 10:
+        nota = ("Digite uma nota entre 0 á 10: ")
+    else:
+        break
