89a8c7ce1d09f18c8309ee1dab174214a531e6f9
Amanda1507 <lilhithrose@gmail.com>
2026-02-02 17:21:05 -0300

    Add files via upload

--- exercicios-7/ex2.py
+++ exercicios-7/ex2.py
@@ -1,35 +1,47 @@
 def cambio():
-    valor = float(input("Qual valor deseja converter? "))
+    while True:
+        print("\n--- Conversor de Moedas ---")
+        opcao = int(input("Qual moeda você TEM agora?\n0 - Sair\n1 - Reais\n2 - Dólar\n3 - Libra\nOpção: "))
 
-    opcao = int(input(f"Qual moeda você Tem agora? \n 1 - Reais \n 2 - Dólar \n 3 - Libra \n Opção: "))
+        if opcao == 0:
+            print("Saindo do programa...")
+            break
+        
+        if opcao not in [1, 2, 3]:
+            print("Opção inválida! Tente novamente.")
+            continue
 
-    if opcao == 1:
-        opn = int(input(f"Para qual moeda quer converter os {valor} R$? 1 - Dólar| 2 - Libra "))
-        if opn == 1:
-            print (f"O valor convertido é: {valor/5.20:.2f} R$")
-        
+        valor = float(input("Qual valor deseja converter?"))
+
+        if opcao == 1:
+            opn = int(input(f"Para qual moeda quer converter os {valor} R$? 1 - Dólar| 2 - Libra "))
+            if opn == 1:
+                print (f"O valor convertido é: {valor/5.20:.2f} R$")
+            
+            else:
+                print (f"O valor convertido é: {valor/7.16:.2f} R$")
+            
+        elif opcao == 2:
+            opnn = int(input(f"Para qual moeda quer converter os {valor} U$? 1 - Real| 2 - Libra "))
+            if opnn == 1:
+                print (f"O valor convertido é: {valor*5.20:.2f} $")
+            
+            else: 
+                print (f"O valor convertido é: {valor*0.78:.2f} $")
+            
+        elif opcao == 3:
+            opnnn = int(input(F"Para qual moeda quer converter as {valor}C? 1- Real| 2 - Dólar "))
+            if opnnn == 1:
+                print (f"O valor convertido é: {valor*7.16:.2f} £")
+            
+            else:
+                print (f"O valor convertido é: {valor*1.25:.2f} £")
+            
         else:
-            print (f"O valor convertido é: {valor/7.16:.2f} R$")
-        
-    elif opcao == 2:
-        opnn = int(input(f"Para qual moeda quer converter os {valor} U$? 1 - Real| 2 - Libra "))
-        if opnn == 1:
-            print (f"O valor convertido é: {valor/5.20:.2f} U$")
-        
-        else: 
-            print (f"O valor convertido é: {valor*1.25:.2f} U$")
-        
-    elif opcao == 3:
-        opnnn = int(input(F"Para qual moeda quer converter as {valor}C? 1- Real| 2 - Dólar "))
-        if opnnn == 1:
-            print (f"O valor convertido é: {valor/7.16:.2f} C")
-        
-        else:
-            print (f"O valor convertido é: {valor*1.25:.2f} C")
-        
-    else:
-        print("Opção inválida")
-        
+            print("Opção inválida")
+
+
+            
 
 
 cambio()
